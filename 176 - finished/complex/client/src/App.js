941ee3aa241f2da43948907e4ab5e4217cf9b5af
AHoier14 <aho@novataris.com>
2022-10-26 13:02:44 +0200

    initial commit

--- 176 - finished/complex/client/src/App.js
+++ 176 - finished/complex/client/src/App.js
@@ -11,24 +11,24 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Fib Calculator version 2</Link>
-          <Link to="/otherpage">Other Page</Link>
-          <div>
-            <Route exact path="/" component={Fib}/>
-            <Route path="/otherpage" component={OtherPage}/>
-          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Calculator
           </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
